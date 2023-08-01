d5a18d71312cf66b979a521887edf725c91f3115
KotaMorikawa <koke.kokota0130@gmail.com>
2023-08-01 23:12:26 +0900

    initial commit

---
+++ x/blog/client/cli/query_post_count.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+
+	"blog/x/blog/types"
+)
+
+func CmdShowPostCount() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-post-count",
+		Short: "shows postCount",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryGetPostCountRequest{}
+
+			res, err := queryClient.PostCount(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
